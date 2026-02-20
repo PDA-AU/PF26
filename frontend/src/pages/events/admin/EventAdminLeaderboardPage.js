2837aaae4e2157dc7b6bc6e71f6774552b46ac8c
YajivGK <vijaygkindian.com>
2026-02-20 19:37:39 +0530

    pdf export

--- frontend/src/pages/events/admin/EventAdminLeaderboardPage.js
+++ frontend/src/pages/events/admin/EventAdminLeaderboardPage.js
@@ -515,6 +515,7 @@ function LeaderboardContent() {
                 if (filters.gender) params.append('gender', filters.gender);
                 if (filters.batch) params.append('batch', filters.batch);
             }
+            if (sortOption) params.append('sort', sortOption);
             (filters.roundIds || []).forEach((roundId) => {
                 params.append('round_ids', String(roundId));
             });
